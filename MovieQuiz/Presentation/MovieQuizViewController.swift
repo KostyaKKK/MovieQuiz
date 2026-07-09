5710777d41bba8bebaa1c9829caa366817a5b9da
Kostya <kashcheev271292vh52183@gmail.com>
2026-07-09 13:10:52 +0300

    переделка moviequiz и добавление статистики в alert

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -2,10 +2,151 @@ import UIKit
 
 final class MovieQuizViewController: UIViewController {
     // MARK: - Lifecycle
-    override func viewDidLoad() {
+    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet private var textLabel: UILabel!
+    @IBOutlet private var counterLabel: UILabel!
+    private var currentQuestionIndex = 0
+    private var correctAnswers = 0
+    private var gamesCount = 0
+    private var bestScore = 0
+    private var bestScoreData = ""
+    private var totalCorrectAnswers = 0
+    private let questions: [QuizQuestion] = [
+        QuizQuestion(image: "The Godfather", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "The Dark Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "Kill Bill", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "The Avengers", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "Deadpool", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "The Green Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "Old", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+        QuizQuestion(image: "The Ice Age Adventures of Buck Wild", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+        QuizQuestion(image: "Tesla", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+        QuizQuestion(image: "Vivarium", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false)
+    ]
+    override func viewDidLoad() { // Life
         super.viewDidLoad()
+        let firstQuestion = questions[currentQuestionIndex]
+        let viewModel = convert (model: firstQuestion)
+        show (quiz: viewModel)
     }
+    @IBAction func noButtonClicked(_ sender: UIButton) {
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = false
+        
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+    }
+    @IBAction func yesButtonClicked(_ sender: UIButton) {
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = true
+        
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+    }
+    private func showAnswerResult(isCorrect: Bool) {
+        if isCorrect {
+            correctAnswers += 1
+        }
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 8
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor: UIColor.ypRed.cgColor
+        imageView.layer.cornerRadius = 20
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.showNextQuestionResults()
+        }
+    }
+    private func showNextQuestionResults() {
+        if currentQuestionIndex == questions.count - 1 {
+            gamesCount += 1
+            totalCorrectAnswers += correctAnswers
+            if correctAnswers >= bestScore {
+                bestScore = correctAnswers
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "dd.MM.yyyy HH:mm"
+                bestScoreData = dateFormatter.string(from: Date())
+            }
+        let totalQuestionAsked = gamesCount * questions.count
+            let averageScore = Double(totalCorrectAnswers) / Double(totalQuestionAsked) * 100
+            
+            
+            let text = """
+            Ваш результат: \(correctAnswers)/\(questions.count)
+            Количество сыгранных квизов: \(gamesCount)
+            Рекорд: \(bestScore)/\(questions.count) (\(bestScoreData))
+            Средняя точность:   \(String(format: "%.2f", averageScore))%
+            """
+            let viewModel = QuizResultsViewModel(
+                title: "Этот раунд окончен!",
+                text: text,
+                buttonText: "Сыграть ещё раз")
+            show(quiz: viewModel) // 3
+        } else {
+            currentQuestionIndex += 1
+            let nextQuestion = questions[currentQuestionIndex]
+            let viewModel = convert(model: nextQuestion)
+            
+            show(quiz: viewModel)
+            imageView.layer.borderWidth = 0
+        }
+    }
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        let questionImage = UIImage(named: model.image) ?? UIImage()
+        return QuizStepViewModel (
+            image: questionImage,
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
+    }
+    private func show(quiz step: QuizStepViewModel) {
+        imageView.image = step.image
+        textLabel.text = step.question
+        counterLabel.text = step.questionNumber
+    }
+    private func show(quiz result: QuizResultsViewModel) {
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.text,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+
+                self.currentQuestionIndex = 0
+                self.correctAnswers = 0
+                
+                let firstQuestion = self.questions[self.currentQuestionIndex]
+                let viewModel = self.convert(model: firstQuestion)
+                self.show(quiz: viewModel)
+                self.imageView.layer.borderWidth = 0
+            }
+        
+        alert.addAction(action)
+        
+        self.present(alert, animated: true, completion: nil)
+        imageView.layer.borderWidth = 0
+        self.imageView.layer.borderWidth = 0
+    }
+    private func showNetworkError(message: String) {
+        imageView.layer.borderWidth = 0
+        let viewModel = QuizResultsViewModel(title: "Что-то пошло не так(", text: message, buttonText: "Попробовать еще раз")
+        show(quiz: viewModel)
+    }
+    struct QuizStepViewModel {
+        let image: UIImage
+        let question: String
+        let questionNumber: String
+    }
+    
+    struct QuizQuestion {
+        let image: String
+        let text: String
+        let correctAnswer: Bool
+    }
+    struct QuizResultsViewModel {
+        let title: String
+        let text: String
+        let buttonText: String
+    }
+    
 }
+
 
 /*
  Mock-данные
